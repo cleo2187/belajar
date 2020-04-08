ae62d9fe1cfb3d1b600775eb6bb2d3e337c8abe3
cleo2187 <60935543+cleo2187@users.noreply.github.com>
2020-04-09 04:17:44 +0700

    Create belajar.py
    i am study otodidac form making scripts tools termux

---
+++ belajar.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python2
+#coding=utf-8
+
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllibe,cookielib,request,mechanize
+form multiprocessin.pool import.threadpoll
+form request.exceptions import connectionerror
+from mechanize import Browser
